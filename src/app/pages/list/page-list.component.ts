4f9301f3c09bee9e3d432889c2045a42cdb1c636
Hayri Mert kurt <hayrimertkurt@outlook.com>
2024-07-19 17:44:41 +0300

    +Client side paging was added.

--- src/app/pages/list/page-list.component.ts
+++ src/app/pages/list/page-list.component.ts
@@ -1,11 +1,11 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { APIService } from '../../Services/api.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatSelectModule } from '@angular/material/select';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
@@ -15,9 +15,8 @@ import { MatInputModule } from '@angular/material/input';
 import { AddingDialogComponent } from '../dialog/adding-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmationDialogComponent } from '../../confirmation-dialog/confirmation-dialog.component';
-
-
-
+import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-page-list',
@@ -36,18 +35,16 @@ import { ConfirmationDialogComponent } from '../../confirmation-dialog/confirmat
     MatDialogModule,
     MatInputModule,
     AddingDialogComponent,
-
+    MatPaginatorModule
   ],
-
   templateUrl: './page-list.component.html',
-  styleUrl: './page-list.component.scss'
+  styleUrls: ['./page-list.component.scss']
 })
-
-
-export class PageListComponent implements OnInit {
+export class PageListComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   PageDataSource: any = [];
-  PageDatas: any;
+  PageDatas = new MatTableDataSource<any>();
   PageDatasId: any;
   removeResult: any;
   selected = 'option2';
@@ -57,17 +54,15 @@ export class PageListComponent implements OnInit {
   selectedPage: number = -1;
   displayedColumns: string[] = [];
 
-  name: string = ''; // Add property for name
-  animal: string = ''; // Add property for animal
-  favoriteColor: string = ''; // Add property for favoriteColor
+  name: string = '';
+  animal: string = '';
+  favoriteColor: string = '';
   messageForChild: string = "hello";
   pageResult: any;
-  pageParamIdResult:any;
-  editRow:any;
-  currentPageParamIds:any;
-  updateRowId:any;
-
-
+  pageParamIdResult: any;
+  editRow: any;
+  currentPageParamIds: any;
+  updateRowId: any;
 
   constructor(
     private apiService: APIService,
@@ -79,45 +74,33 @@ export class PageListComponent implements OnInit {
   ngOnInit(): void {
     this.apiService.GetAllPages().subscribe(result => {
       this.PageDataSource = result;
-      
     });
+  }
+
+  ngAfterViewInit() {
+    this.PageDatas.paginator = this.paginator;
   }
 
   GetPageValues(): any {
     if (this.selectedPage == -1) { return; }
-    console.log(this.selectedPage);
+
     this.apiService.GetPageValuesById(this.selectedPage).subscribe(pageResult => {
-
-      this.PageDatas = pageResult;
-
-      pageResult.forEach((element: { Id: any; }) => 
-      {
-        console.log('Id:', element.Id);
-      });
-
-      this.displayedColumns = Object.keys(this.PageDatas[0]);
+      this.PageDatas.data = pageResult;
+      this.displayedColumns = Object.keys(pageResult[0]);
       this.displayedColumns.push("Actions");
 
       this.apiService.GetAllParamIDs(this.selectedPage).subscribe(result => {
-        this.currentPageParamIds=result;
-        console.table(this.currentPageParamIds);
-        
+        this.currentPageParamIds = result;
         this.changeDedector.detectChanges();
       });
       this.changeDedector.detectChanges();
-
-
-
-    });   
-    // this.apiService.GetAllParamIDs(1).subscribe(resultParamIds => {
-    //   this.pageParamIdResult=resultParamIds;
-    //   console.table(this.pageParamIdResult)
-    // });
+    });
   }
 
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action);
   }
+
   deleteElement(rowId: any) {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
       width: '400px',
@@ -136,46 +119,44 @@ export class PageListComponent implements OnInit {
   }
    
   openDialog(): void {
-      let dialogRef = this.dialog.open(AddingDialogComponent, {
-        data: {
-            currentParameterIds: this.currentPageParamIds, currentPage: this.PageDataSource[this.selectedPage - 1],
-            selectedPageId: this.selectedPage,
-            pagedata: this.PageDatas,
-            updateRow: null,
-            updateRowId:null
-          }
-      });
-      
-      dialogRef.afterClosed().subscribe(result => {
-          this.apiService.AddParameterValue(result).subscribe(paramResult => {
-          this.GetPageValues();
-        });
-      })
-
-  }
-
-  editElement(rowId: any) {
-    this.editRow = this.PageDatas.find((data: { Id: any; }) => data.Id === rowId);
-    console.log(this.editRow);
-    this.updateRowId = rowId;
-  
     let dialogRef = this.dialog.open(AddingDialogComponent, {
       data: {
         currentParameterIds: this.currentPageParamIds,
         currentPage: this.PageDataSource[this.selectedPage - 1],
         selectedPageId: this.selectedPage,
-        pagedata: this.PageDatas,
+        pagedata: this.PageDatas.data,
+        updateRow: null,
+        updateRowId: null
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.apiService.AddParameterValue(result).subscribe(() => {
+        this.GetPageValues();
+      });
+    });
+  }
+
+  editElement(rowId: any) {
+    this.editRow = this.PageDatas.data.find((data: { Id: any }) => data.Id === rowId);
+    console.log(this.editRow);
+    this.updateRowId = rowId;
+
+    let dialogRef = this.dialog.open(AddingDialogComponent, {
+      data: {
+        currentParameterIds: this.currentPageParamIds,
+        currentPage: this.PageDataSource[this.selectedPage - 1],
+        selectedPageId: this.selectedPage,
+        pagedata: this.PageDatas.data,
         updateRow: this.editRow,
         updateRowId: this.updateRowId,
       }
     });
-  
+
     dialogRef.afterClosed().subscribe(result => {
-      this.apiService.UpdateParameterValue(this.updateRowId,this.selectedPage, result).subscribe(updateResult=>{
+      this.apiService.UpdateParameterValue(this.updateRowId, this.selectedPage, result).subscribe(() => {
         this.GetPageValues();
       });
-      
     });
   }
-  
 }
